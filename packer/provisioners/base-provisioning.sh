2a6fe028e10c38269b89f6b3e5eb610dca9f6fa0
Urs Voegele <unext@bluewin.ch>
2021-03-15 16:19:13 +0100

    new basebox version 2.0.1615821186

--- packer/provisioners/base-provisioning.sh
+++ packer/provisioners/base-provisioning.sh
@@ -9,6 +9,9 @@ application_file_path="/vagrant/installed-application.md"
 # set ls directory color
 sudo echo "export LS_COLORS+=':ow=0;33'" >> /home/vagrant/.bashrc
 
+# disable unattended upgrade
+sudo echo -e "APT::Periodic::Update-Package-Lists \"0\";\nAPT::Periodic::Unattended-Upgrade \"0\";\n" > /etc/apt/apt.conf.d/20auto-upgrades
+
 # set timezone
 sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Europe/Zurich /etc/localtime
--- set-build-env.ps1
+++ set-build-env.ps1
@@ -1,4 +1,4 @@
 
 $env:SRC_IMAGE_NAME = 'elegoev/ubuntu-18.04'
-$env:SRC_IMAGE_VERSION = '2.0.1614701845'
+$env:SRC_IMAGE_VERSION = '2.0.1615821186'
 
